b8b5aa86e1e55fd70f4fbbaea0eb47abc97d0cd1
to4to <justjustifying@outlook.com>
2024-07-16 22:47:41 +0530

    chore: Refactor PreSale contract to ICOMarketPlace contract
    Refactor the PreSale contract to ICOMarketPlace contract to better reflect its purpose and functionality. This includes renaming the contract, updating the struct name, and modifying the function names accordingly. The changes also include adding modifiers for supporting tokens, restricting access to only the owner and creator, and implementing a receive function for accepting payments. Additionally, the createICOSale function has been added to create an ICO sale with a specified token and price. The multiply function has also been moved to an internal function.
    
    Recent user commits:
    - chore: Update Pinata API key and secret key
    - chore: Initialize project structure and dependencies

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -3,8 +3,44 @@ pragma solidity ^0.8.20;
 
 
 
-contract PreSale {
-    constructor() {
+contract ICOMarketPlace {
+    struct  TokenDetails {
+        uint256 test;
+    }
+
+
+
+    //Modifiers
+
+
+    modifier supportToken() {
         
     }
+
+
+    modifier onlyOwner(){}
+
+    modifier onlyCreator(){}
+
+
+
+    receive() external payable{}
+
+
+
+    constructor(){} 
+
+
+
+    //functions
+
+
+    function createICOSale(address _token,uint256 price)  {
+        
+    }
+
+
+
+
+    function multiply(uint256 x,uint256 y)internal pure returns(uint256 z){}
 }
